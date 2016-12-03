3702d82ff2b216427bf17fa82b69cac50675e902
theaustinliou <aus.liou@gmail.com>
2016-12-03 02:18:26 +0000

    added tests

---
+++ tests/cd_test.sh
@@ -0,0 +1,8 @@
+cd src # will be in src dir
+cd - # will be in rshell
+cd # will be in HOME
+cd tests # will be in tests
+cd .. # will be in rshell
+cd foo #ERROR: couldn't change directory
+cd . #will stay in same dir
+cd - # will go back one dir
--- tests/commented_command.sh
+++
@@ -1,6 +0,0 @@
-echo A #&& test rshell.cpp 
-test rshell.cpp && echo A #|| echo B
-test abc.cpp #; test rshell.cpp
-(echo A && echo B) #&& echo C
-test -d rshell && (echo A && echo B) #|| test a.out
-#test rshell.cpp && test a.out
